e6d687cd4d2cbf941d2342e69e81acf06e98232d
YeongjinOh <oyj9109@snu.ac.kr>
2016-07-12 20:26:55 +0900

    basic operation

---
+++ practice/0.Basic_operation/prac1.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+
+sess = tf.Session()
+
+a = tf.constant(2)
+b = tf.constant(3)
+
+c = a+b
+
+print c
+
+print sess.run(c)
+
---
+++ practice/0.Basic_operation/prac2.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+a = tf.placeholder(tf.int16)
+b = tf.placeholder(tf.int16)
+
+add = tf.add(a, b)
+mul = tf.mul(a, b)
+
+print add
+print mul
+print ""
+
+with tf.Session() as sess:
+        print "Addition with variables:%i" % sess.run(add, feed_dict={a:2, b:3})
+        print "Multiplication with variables:%i" % sess.run(mul, feed_dict={a:4, b:3})
+
+
